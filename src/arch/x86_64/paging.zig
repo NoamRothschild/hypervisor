d76c308720b18db13c883ca6eb9f6f1e22bf132f
NoamRothschild <noamr.lego@gmail.com>
2026-08-30 10:45:29 +0300

    ref: re-organized paging structures

--- src/arch/x86_64/paging.zig
+++ src/arch/x86_64/paging.zig
@@ -30,111 +30,232 @@ pub const PML4E = packed struct(u64) {
             .xd = 0,
         };
     }
+
+    // NOTE: idk if I like this or not yet
+    pub inline fn present(self: *const @This()) linksection(".text.boot") bool {
+        return self.p != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) linksection(".text.boot") void {
+        self.p = @intFromBool(is_present);
+    }
 };
 
-pub const PDPTE = packed struct(u64) {
+pub const PDPTE = packed union(u64) {
+    PD: packed struct(u64) {
+        p: u1,
+        r_w: u1,
+        u_s: u1,
+        pwt: u1,
+        pcd: u1,
+        a: u1 = 0,
+        avl1: u1 = 0,
+        ps: u1 = 0,
+        avl2: u4 = 0,
+        /// this is phys addr truncated (physical >> 12)
+        /// stores the addr of a PD
+        phys_addr: u40,
+        // rsvd: u0 = 0
+        avl3: u11 = 0,
+        xd: u1,
+
+        pub fn kernel_page(phys_addr: u64) linksection(".text.boot") @This() {
+            return .{
+                .p = 1,
+                .r_w = 1,
+                .u_s = 0,
+                .pwt = 0,
+                .pcd = 0,
+                .phys_addr = @truncate(phys_addr >> 12),
+                .xd = 0,
+            };
+        }
+    },
+    @"1 GB": packed struct(u64) {
+        p: u1,
+        r_w: u1,
+        u_s: u1,
+        pwt: u1,
+        pcd: u1,
+        a: u1 = 0,
+        d: u1 = 0,
+        ps: u1 = 1,
+        g: u1,
+        avl1: u3 = 0,
+        pat: u1,
+        rsvd: u17 = 0,
+        /// this is phys addr truncated (physical >> 30)
+        /// stores the physical addr in RAM
+        phys_addr: u22,
+        avl2: u7 = 0,
+        pk: u4,
+        xd: u1,
+
+        pub fn kernel_page(phys_addr: u22) linksection(".text.boot") @This() {
+            return .{
+                .p = 1,
+                .ps = 1,
+                .r_w = 1,
+                .u_s = 0,
+                .pwt = 0,
+                .pcd = 0,
+                .pat = 0,
+                .g = 0,
+                .pk = 0,
+                .phys_addr = @truncate(phys_addr),
+                .xd = 0,
+            };
+        }
+    },
+
+    pub fn kernel_page(comptime tag: enum { PD, @"1 GB" }, phys_addr: u64) linksection(".text.boot") @This() {
+        return switch (tag) {
+            .PD => .{ .PD = .kernel_page(phys_addr) },
+            .@"1 GB" => .{ .@"1 GB" = .kernel_page(@truncate(phys_addr)) },
+        };
+    }
+
+    // NOTE: idk if I like this or not yet
+    pub inline fn present(self: *const @This()) linksection(".text.boot") bool {
+        return self.PD.p != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) linksection(".text.boot") void {
+        self.PD.p = @intFromBool(is_present);
+    }
+};
+
+pub const PDE = packed union(u64) {
+    @"4 KB": packed struct(u64) {
+        p: u1,
+        r_w: u1,
+        u_s: u1,
+        pwt: u1,
+        pcd: u1,
+        a: u1 = 0,
+        avl1: u1 = 0,
+        ps: u1 = 0,
+        avl2: u4 = 0,
+        /// this is phys addr truncated (physical >> 12)
+        /// stores the addr of a PT
+        phys_addr: u40,
+        // rsvd: u0 = 0
+        avl3: u11 = 0,
+        xd: u1,
+
+        pub fn kernel_page(phys_addr: u64) linksection(".text.boot") @This() {
+            return .{
+                .p = 1,
+                .r_w = 1,
+                .u_s = 0,
+                .pwt = 0,
+                .pcd = 0,
+                .phys_addr = @truncate(phys_addr >> 12),
+                .xd = 0,
+            };
+        }
+    },
+    @"2 MB": packed struct(u64) {
+        p: u1,
+        r_w: u1,
+        u_s: u1,
+        pwt: u1,
+        pcd: u1,
+        a: u1 = 0,
+        d: u1 = 0,
+        ps: u1 = 1,
+        g: u1,
+        avl1: u3 = 0,
+        pat: u1,
+        rsvd: u8 = 0,
+        /// this is phys addr truncated (physical >> 21)
+        /// stores the physical addr in RAM
+        phys_addr: u31,
+        // rsvd: u0 = 0
+        avl2: u7 = 0,
+        pk: u4,
+        xd: u1,
+
+        pub fn kernel_page(phys_addr: u31) linksection(".text.boot") @This() {
+            return .{
+                .p = 1,
+                .r_w = 1,
+                .u_s = 0,
+                .pwt = 0,
+                .pcd = 0,
+                .g = 0,
+                .pat = 0,
+                .phys_addr = phys_addr,
+                .pk = 0,
+                .xd = 0,
+            };
+        }
+    },
+
+    pub fn kernel_page(comptime page_size: enum { @"4 KB", @"2 MB" }, phys_addr: u64) linksection(".text.boot") @This() {
+        return switch (page_size) {
+            .@"4 KB" => .{ .@"4 KB" = .kernel_page(phys_addr) },
+            .@"2 MB" => .{ .@"2 MB" = .kernel_page(@truncate(phys_addr)) },
+        };
+    }
+
+    // NOTE: idk if I like this or not yet
+    pub inline fn present(self: *const @This()) linksection(".text.boot") bool {
+        return self.@"4 KB".p != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) linksection(".text.boot") void {
+        self.@"4 KB".p = @intFromBool(is_present);
+    }
+};
+
+pub const PTE = packed struct(u64) {
     p: u1,
     r_w: u1,
     u_s: u1,
     pwt: u1,
     pcd: u1,
     a: u1 = 0,
-    avl1: u1 = 0,
-    ps: u1 = 0,
-    avl2: u4 = 0,
+    d: u1 = 0,
+    pat: u1,
+    g: u1,
+    avl1: u3 = 0,
     /// this is phys addr truncated (physical >> 12)
-    /// stores the addr of a PD
+    /// stores the physical addr in RAM
     phys_addr: u40,
-    // rsvd: u0 = 0
-    avl3: u11 = 0,
+    avl2: u7 = 0,
+    pk: u4,
     xd: u1,
 
     pub fn kernel_page(phys_addr: u64) linksection(".text.boot") @This() {
-        return PDPTE{
+        return PTE{
             .p = 1,
             .r_w = 1,
             .u_s = 0,
             .pwt = 0,
             .pcd = 0,
+            .pat = 0,
+            .g = 0,
             .phys_addr = @truncate(phys_addr >> 12),
-            .xd = 0,
-        };
-    }
-};
-
-pub const PDPTE_1GB = packed struct(u64) {
-    p: u1,
-    r_w: u1,
-    u_s: u1,
-    pwt: u1,
-    pcd: u1,
-    a: u1 = 0,
-    d: u1 = 0,
-    ps: u1 = 1,
-    g: u1,
-    avl1: u3 = 0,
-    pat: u1,
-    rsvd: u17 = 0,
-    /// this is phys addr truncated (physical >> 30)
-    /// stores the physical addr in RAM
-    phys_addr: u22,
-    avl2: u7 = 0,
-    pk: u4,
-    xd: u1,
-
-    pub fn kernel_page(phys_addr: u22) linksection(".text.boot") @This() {
-        return PDPTE_1GB{
-            .p = 1,
-            .ps = 1,
-            .r_w = 1,
-            .u_s = 0,
-            .pwt = 0,
-            .pcd = 0,
-            .pat = 0,
-            .g = 0,
-            .pk = 0,
-            .phys_addr = @truncate(phys_addr),
-            .xd = 0,
-        };
-    }
-};
-
-pub const PDE = packed struct(u64) {
-    p: u1,
-    r_w: u1,
-    u_s: u1,
-    pwt: u1,
-    pcd: u1,
-    a: u1 = 0,
-    d: u1 = 0,
-    ps: u1 = 1,
-    g: u1,
-    avl1: u3 = 0,
-    pat: u1,
-    rsvd: u8 = 0,
-    /// this is phys addr truncated (physical >> 21)
-    /// stores the physical addr in RAM
-    phys_addr: u31,
-    // rsvd: u0 = 0
-    avl2: u7 = 0,
-    pk: u4,
-    xd: u1,
-
-    pub fn kernel_page(phys_addr: u31) linksection(".text.boot") @This() {
-        return PDE{
-            .p = 1,
-            .r_w = 1,
-            .u_s = 0,
-            .pwt = 0,
-            .pcd = 0,
-            .g = 0,
-            .pat = 0,
-            .phys_addr = phys_addr,
             .pk = 0,
             .xd = 0,
         };
     }
+
+    // NOTE: idk if I like this or not yet
+    pub inline fn present(self: *const @This()) linksection(".text.boot") bool {
+        return self.p != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) linksection(".text.boot") void {
+        self.p = @intFromBool(is_present);
+    }
 };
+
+pub const PDPTE_PD = @TypeOf(@as(PDPTE, @bitCast(@as(u64, 0))).PD);
+pub const PDPTE_1GB = @TypeOf(@as(PDPTE, @bitCast(@as(u64, 0))).@"1 GB");
+pub const PDE_4KB = @TypeOf(@as(PDE, @bitCast(@as(u64, 0))).@"4 KB");
+pub const PDE_2MB = @TypeOf(@as(PDE, @bitCast(@as(u64, 0))).@"2 MB");
 
 extern var kernel_physical_start: u8;
 extern var kernel_size_in_4KIB_pages: u8;
@@ -142,7 +263,7 @@ extern var kernel_size_in_4KIB_pages: u8;
 pub extern var PML4T: [512]PML4E align(0x1000) linksection(".bss.boot");
 
 var kernelPDPT: [512]PDPTE align(0x1000) linksection(".bss.boot") = undefined;
-var kernelPD: [512]PDE align(0x1000) linksection(".bss.boot") = undefined;
+var kernelPD: [512]PDE_2MB align(0x1000) linksection(".bss.boot") = undefined;
 var last_allocated_kernel_directory_page: usize linksection(".bss.boot") = 0;
 
 pub const higher_half_base: comptime_int = 0xFFFFFFFF80000000;
@@ -180,12 +301,12 @@ pub fn init() linksection(".text.boot") callconv(.c) void {
     for (0..kernel_size_2MIB_pages) |i| {
         const pdei = i + kernel_physical_start_pde_idx;
 
-        kernelPD[pdei] = PDE.kernel_page(@truncate(pdei));
+        kernelPD[pdei] = .kernel_page(@truncate(pdei));
     }
     // last pdei value
     last_allocated_kernel_directory_page = kernel_size_2MIB_pages - 1 + kernel_physical_start_pde_idx;
 
-    kernelPDPT[kernel_pdpt_idx] = PDPTE.kernel_page(@intFromPtr(&kernelPD));
+    kernelPDPT[kernel_pdpt_idx] = PDPTE.kernel_page(.PD, @intFromPtr(&kernelPD));
     // only update PML4 once everything is set up
     PML4T[kernel_pml4_idx] = PML4E.kernel_page(@intFromPtr(&kernelPDPT));
 
@@ -217,7 +338,7 @@ pub fn allocPage() !u64 {
     last_allocated_kernel_directory_page +%= 1;
 
     const new_pd_idx = last_allocated_kernel_directory_page;
-    kernelPD[new_pd_idx] = PDE.kernel_page(last_allocated_phys +% 1);
+    kernelPD[new_pd_idx] = .kernel_page(last_allocated_phys +% 1);
 
     return (new_pd_idx << 21) | (kernel_pdpt_idx << 30) | (kernel_pml4_idx << 39) | (0xffff << 48);
 }
@@ -284,13 +405,10 @@ pub const Allocator4K = struct {
     }
 };
 
-var alloc_4k_aligned_state: ?Allocator4K = null;
+var alloc_4k_aligned_state: Allocator4K = .empty;
 
 /// allocates and returns an aligned virtual addr of a free 4KiB page.
 ///  virtual addr lives inside the higher half mapping
 pub fn alloc4KAligned() !*align(4096) [4096]u8 {
-    if (alloc_4k_aligned_state == null)
-        alloc_4k_aligned_state = Allocator4K.empty;
-
-    return @ptrFromInt(try alloc_4k_aligned_state.?.next());
+    return @ptrFromInt(try alloc_4k_aligned_state.next());
 }
