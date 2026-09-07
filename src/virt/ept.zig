6861199bf098f9997e6de1c3767ea8d84a01beb2
NoamRothschild <noamr.lego@gmail.com>
2026-09-07 11:31:15 +0300

    EPT: organized table structure formats
    similar format to src/arch/x86_64/paging.zig

--- src/virt/ept.zig
+++ src/virt/ept.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 const paging = @import("../arch/x86_64/paging.zig");
 const vmx = @import("vmx.zig");
-// set the 7th bit of PDPTE to map a 1 GB page
-// set the 7th bit of PDE to maps a 2 MB page
-// note that in both scenarios the structure of the entry will differ. look in the intel manual for it.
+// EPT tables map guest-physical addresses to host-physical addresses.
 
 /// EPT pointer
 pub const EPTP = packed struct(u64) {
@@ -27,12 +25,245 @@ pub const EPT_PML4E = packed struct(u64) {
     rsvd2: u1 = 0,
     exec_for_usermode: u1,
     rsvd3: u1 = 0,
+    /// this is phys addr truncated (physical >> 12)
+    /// stores the addr of an EPT_PDPTE table
     phys_addr: u36,
     rsvd4: u16 = 0,
+
+    pub fn makeEntry(phys_addr: u64) @This() {
+        return .{
+            .read = 1,
+            .write = 1,
+            .execute = 1,
+            .accessed = 0,
+            .exec_for_usermode = 0,
+            .phys_addr = @truncate(phys_addr >> 12),
+        };
+    }
+
+    pub inline fn initNotPresent() @This() {
+        var entry: @This() = @bitCast(@as(u64, undefined));
+        entry.setPresent(false);
+        return entry;
+    }
+
+    pub inline fn present(self: *const @This()) bool {
+        return self.read != 0 or self.write != 0 or self.execute != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) void {
+        const bit: u1 = @intFromBool(is_present);
+        self.read = bit;
+        self.write = bit;
+        self.execute = bit;
+    }
+
+    pub inline fn physAddr(self: *const @This()) u64 {
+        return @as(u64, self.phys_addr) << 12;
+    }
 };
 
-pub const EPT_PDPTE = EPT_PML4E;
-pub const EPT_PDE = EPT_PML4E;
+pub const EPT_PDPTE = packed union(u64) {
+    PD: packed struct(u64) {
+        read: u1,
+        write: u1,
+        execute: u1,
+        rsvd1: u4 = 0,
+        page_size: u1 = 0,
+        accessed: u1,
+        rsvd2: u1 = 0,
+        exec_for_usermode: u1,
+        rsvd3: u1 = 0,
+        /// this is phys addr truncated (physical >> 12)
+        /// stores the addr of an EPT_PDE table
+        phys_addr: u36,
+        rsvd4: u16 = 0,
+
+        pub fn makeEntry(phys_addr: u36) @This() {
+            return .{
+                .read = 1,
+                .write = 1,
+                .execute = 1,
+                .accessed = 0,
+                .exec_for_usermode = 0,
+                .phys_addr = phys_addr,
+            };
+        }
+
+        pub inline fn physAddr(self: *const @This()) u64 {
+            return @as(u64, self.phys_addr) << 12;
+        }
+    },
+    @"1 GB": packed struct(u64) {
+        read: u1,
+        write: u1,
+        execute: u1,
+        ept_mem_type: u3,
+        ignore_pat: u1,
+        page_size: u1 = 1,
+        accessed: u1,
+        dirty: u1,
+        exec_for_usermode: u1,
+        rsvd1: u1 = 0,
+        rsvd2: u18 = 0,
+        /// this is phys addr truncated (physical >> 30)
+        /// stores the physical addr in guest RAM
+        phys_addr: u22,
+        rsvd3: u11 = 0,
+        supress_ve: u1,
+
+        pub fn makeEntry(phys_addr: u22) @This() {
+            return .{
+                .read = 1,
+                .write = 1,
+                .execute = 1,
+                .ept_mem_type = 6, // Write Back
+                .ignore_pat = 0,
+                .accessed = 0,
+                .dirty = 0,
+                .exec_for_usermode = 0,
+                .phys_addr = phys_addr,
+                .supress_ve = 0,
+            };
+        }
+
+        pub inline fn physAddr(self: *const @This()) u64 {
+            return @as(u64, self.phys_addr) << 30;
+        }
+    },
+
+    pub fn makeEntry(comptime tag: enum { PD, @"1 GB" }, phys_addr: u64) @This() {
+        return switch (tag) {
+            .PD => .{ .PD = .makeEntry(@truncate(phys_addr >> 12)) },
+            .@"1 GB" => .{ .@"1 GB" = .makeEntry(@truncate(phys_addr >> 30)) },
+        };
+    }
+
+    pub inline fn initNotPresent() @This() {
+        var entry: @This() = @bitCast(@as(u64, undefined));
+        entry.setPresent(false);
+        return entry;
+    }
+
+    pub inline fn present(self: *const @This()) bool {
+        return self.PD.read != 0 or self.PD.write != 0 or self.PD.execute != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) void {
+        const bit: u1 = @intFromBool(is_present);
+        self.PD.read = bit;
+        self.PD.write = bit;
+        self.PD.execute = bit;
+    }
+
+    pub inline fn physAddr(self: *const @This(), comptime tag: enum { PD, @"1 GB" }) u64 {
+        return switch (tag) {
+            .PD => self.PD.physAddr(),
+            .@"1 GB" => self.@"1 GB".physAddr(),
+        };
+    }
+};
+
+pub const EPT_PDE = packed union(u64) {
+    PT: packed struct(u64) {
+        read: u1,
+        write: u1,
+        execute: u1,
+        rsvd1: u4 = 0,
+        page_size: u1 = 0,
+        accessed: u1,
+        rsvd2: u1 = 0,
+        exec_for_usermode: u1,
+        rsvd3: u1 = 0,
+        /// this is phys addr truncated (physical >> 12)
+        /// stores the addr of an EPT_PTE table
+        phys_addr: u36,
+        rsvd4: u16 = 0,
+
+        pub fn makeEntry(phys_addr: u36) @This() {
+            return .{
+                .read = 1,
+                .write = 1,
+                .execute = 1,
+                .accessed = 0,
+                .exec_for_usermode = 0,
+                .phys_addr = phys_addr,
+            };
+        }
+
+        pub inline fn physAddr(self: *const @This()) u64 {
+            return @as(u64, self.phys_addr) << 12;
+        }
+    },
+    @"2 MB": packed struct(u64) {
+        read: u1,
+        write: u1,
+        execute: u1,
+        ept_mem_type: u3,
+        ignore_pat: u1,
+        page_size: u1 = 1,
+        accessed: u1,
+        dirty: u1,
+        exec_for_usermode: u1,
+        rsvd1: u1 = 0,
+        rsvd2: u9 = 0,
+        /// this is phys addr truncated (physical >> 21)
+        /// stores the physical addr in guest RAM
+        phys_addr: u31,
+        rsvd3: u11 = 0,
+        supress_ve: u1,
+
+        pub fn makeEntry(phys_addr: u31) @This() {
+            return .{
+                .read = 1,
+                .write = 1,
+                .execute = 1,
+                .ept_mem_type = 6, // Write Back
+                .ignore_pat = 0,
+                .accessed = 0,
+                .dirty = 0,
+                .exec_for_usermode = 0,
+                .phys_addr = phys_addr,
+                .supress_ve = 0,
+            };
+        }
+
+        pub inline fn physAddr(self: *const @This()) u64 {
+            return @as(u64, self.phys_addr) << 21;
+        }
+    },
+
+    pub fn makeEntry(comptime tag: enum { PT, @"2 MB" }, phys_addr: u64) @This() {
+        return switch (tag) {
+            .PT => .{ .PT = .makeEntry(@truncate(phys_addr >> 12)) },
+            .@"2 MB" => .{ .@"2 MB" = .makeEntry(@truncate(phys_addr >> 21)) },
+        };
+    }
+
+    pub inline fn initNotPresent() @This() {
+        var entry: @This() = @bitCast(@as(u64, undefined));
+        entry.setPresent(false);
+        return entry;
+    }
+
+    pub inline fn present(self: *const @This()) bool {
+        return self.PT.read != 0 or self.PT.write != 0 or self.PT.execute != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) void {
+        const bit: u1 = @intFromBool(is_present);
+        self.PT.read = bit;
+        self.PT.write = bit;
+        self.PT.execute = bit;
+    }
+
+    pub inline fn physAddr(self: *const @This(), comptime tag: enum { PT, @"2 MB" }) u64 {
+        return switch (tag) {
+            .PT => self.PT.physAddr(),
+            .@"2 MB" => self.@"2 MB".physAddr(),
+        };
+    }
+};
 
 pub const EPT_PTE = packed struct(u64) {
     read: u1,
@@ -45,21 +276,48 @@ pub const EPT_PTE = packed struct(u64) {
     dirty: u1,
     exec_for_usermode: u1,
     rsvd2: u1 = 0,
+    /// this is phys addr truncated (physical >> 12)
+    /// stores the physical addr in guest RAM
     phys_addr: u36,
     rsvd3: u15 = 0,
     supress_ve: u1,
-};
 
-pub fn makeEntry(comptime T: type, phys_addr: u64) T {
-    return T{
-        .accessed = 0,
-        .execute = 1,
-        .exec_for_usermode = 0,
-        .phys_addr = @truncate(phys_addr >> 12),
-        .read = 1,
-        .write = 1,
-    };
-}
+    pub fn makeEntry(phys_addr: u64) @This() {
+        return .{
+            .read = 1,
+            .write = 1,
+            .execute = 1,
+            .ept_mem_type = 6, // Write Back
+            .ignore_pat = 0,
+            .accessed = 0,
+            .dirty = 0,
+            .exec_for_usermode = 0,
+            .phys_addr = @truncate(phys_addr >> 12),
+            .supress_ve = 0,
+        };
+    }
+
+    pub inline fn initNotPresent() @This() {
+        var entry: @This() = @bitCast(@as(u64, undefined));
+        entry.setPresent(false);
+        return entry;
+    }
+
+    pub inline fn present(self: *const @This()) bool {
+        return self.read != 0 or self.write != 0 or self.execute != 0;
+    }
+
+    pub inline fn setPresent(self: *@This(), is_present: bool) void {
+        const bit: u1 = @intFromBool(is_present);
+        self.read = bit;
+        self.write = bit;
+        self.execute = bit;
+    }
+
+    pub inline fn physAddr(self: *const @This()) u64 {
+        return @as(u64, self.phys_addr) << 12;
+    }
+};
 
 inline fn zeroMem(ptr: anytype, comptime elem_t: type) void {
     const ZeroType = @Int(.unsigned, @bitSizeOf(elem_t));
@@ -84,9 +342,9 @@ pub fn init(guest_state: *vmx.VMState) !void {
     // errdefer free(pt)
     zeroMem(pt, EPT_PTE);
 
-    pd.*[0] = makeEntry(EPT_PDE, paging.physAddr(@intFromPtr(pt)).?);
-    pdpt.*[0] = makeEntry(EPT_PDPTE, paging.physAddr(@intFromPtr(pd)).?);
-    pml4.*[0] = makeEntry(EPT_PML4E, paging.physAddr(@intFromPtr(pdpt)).?);
+    pd.*[0] = .makeEntry(.PT, paging.physAddr(@intFromPtr(pt)).?);
+    pdpt.*[0] = .makeEntry(.PD, paging.physAddr(@intFromPtr(pd)).?);
+    pml4.*[0] = .makeEntry(paging.physAddr(@intFromPtr(pdpt)).?);
     guest_state.eptp = .{
         .dirty_access_enabled = 1,
         .memory_type = 6, // Write Back
@@ -109,18 +367,7 @@ pub fn init(guest_state: *vmx.VMState) !void {
             guest_state.guest_mem_addr = @intFromPtr(guest_mem_sect);
         }
 
-        pt.*[i] = EPT_PTE{
-            .accessed = 0,
-            .dirty = 0,
-            .ept_mem_type = 6,
-            .execute = 1,
-            .exec_for_usermode = 0,
-            .ignore_pat = 0,
-            .phys_addr = @truncate(phys_addr >> 12),
-            .read = 1,
-            .supress_ve = 0,
-            .write = 1,
-        };
+        pt.*[i] = .makeEntry(phys_addr);
     }
 
     std.log.info("EPT ptr stored inside guest_state\n", .{});
